209480c52145ae9b50e986d324b9825d323b9139
aidanperez2000 <aidan@perezwitt.com>
2025-10-04 14:39:13 -0400

    Use Getter and Setter Methods in Static Tile Methods

--- Tile.java
+++ Tile.java
@@ -28,7 +28,7 @@ public class Tile extends Piece {
     * returns tile from list where value is -1
     * throws RuntimeException if no empty tile is found*/
     public static Tile GetEmptyTile(List<Tile> tiles) {
-        Tile emptyTile = tiles.stream().filter(n -> n.Value == -1).findFirst().orElse(null);
+        Tile emptyTile = tiles.stream().filter(n -> n.GetValue() == -1).findFirst().orElse(null);
         if (emptyTile == null)
             throw new RuntimeException("No empty tile found");
         return emptyTile;
@@ -41,8 +41,8 @@ public class Tile extends Piece {
     * next to the empty tile or in the same column and next to the empty tile*/
     public static List<Tile> GetPossibleSwaps(List<Tile> tiles, Tile emptyTile) {
         return tiles.stream().filter(
-                        n -> (n.XPos == emptyTile.XPos && (n.YPos == emptyTile.YPos - 1 || n.YPos == emptyTile.YPos + 1))
-                                || (n.YPos == emptyTile.YPos && (n.XPos == emptyTile.XPos - 1 || n.XPos == emptyTile.XPos + 1)))
+                        n -> (n.GetXPos() == emptyTile.GetXPos() && (n.GetYPos() == emptyTile.GetYPos() - 1 || n.GetYPos() == emptyTile.GetYPos() + 1))
+                                || (n.GetYPos() == emptyTile.GetYPos() && (n.GetXPos() == emptyTile.GetXPos() - 1 || n.GetXPos() == emptyTile.GetXPos() + 1)))
                 .collect(Collectors.toList());
     }
 }
